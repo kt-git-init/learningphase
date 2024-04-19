e5c305a959fc3ac0b723f9bcdce68a4967abee75
Kunal Tajne <kunal.tajne96@gmail.com>
2024-04-18 20:24:58 -0400

    Key Checker

--- Code/Project1/colorchanger.js
+++ Code/Project1/colorchanger.js
@@ -1,2 +1,22 @@
-const buttons = document.querySelectorAll('.button')
-console.log(buttons);
+
+// #FUNCTION
+
+function newWord () {
+    console.log("Working")
+}
+newWord()
+
+// #OBJECT
+const obj = {
+    name : "Kunal",
+    surname : "Tajne",
+}
+
+console.log(`My Name is ${obj.name}`)
+
+// #ARRAY
+const arr = [1, 2, 3, 4, 5]
+
+arr.forEach(function (item){
+    console.log(item)
+})
